cb3834521419b45a18b180ce181d6ddfa8d65268
vrushabh <vrushabhp702@gmail.com>
2024-11-18 18:54:40 +0530

    tailwind file added

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+const {register,handlesubmit,watch,formState:{errors},reset} = useForm()
+const onSubmit = data => {
+    console.log(data)
+}
 
 const Registration = () =>{
     return (
-        <div>
+        <div className='flex justify-center lg:h-screen items-center'>
+            <div className='card w-full md:w-96 items-center shadow-2xl bg-base-100'>
+                <form className='card-body w-full lg:w-96'>
+                    <div className='form-control'>
+                        <label className='label'>
+                            <span className='label-text'>Name</span>
+                        </label>
+                        <input {...register("name",{required:true})} 
+                        type='text' placeholder='name' className='input input-bordered'></input>
 
+                    </div>
+                    <div className='form-control'>
+                        <label className='lable'>
+                            <span className='label-text'>Email</span>
+                        </label>
+                        <input type='email' placeholder='email' className='input input-bordered'/>
+                    </div>
+                    <div className='form-control'>
+                        <label className='label'>
+                            <span className='label-text'>Password</span>
+                        </label>
+                        <input type='password' placeholder='password' className='input input-bordered'/>
+                    </div>
+                    <div className='form-control mt-6'>
+                        <button type='submit' className='btn'>Registration</button>
+                    </div>
+                </form>
+                <label className='mt-2'>
+                    Already having an account? <link to='/login' className='btn btn-link text-white underline px-0'>Login</link>
+                </label>
+            </div>
         </div>
-    )
+    );
 };
 
 export default Registration;

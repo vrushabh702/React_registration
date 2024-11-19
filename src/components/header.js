d41ff7236e3684f737516e9674039a32b8140a2e
vrushabh <vrushabhp702@gmail.com>
2024-11-19 15:24:01 +0530

    header adding

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const header = () =>{
+    return(
+        <div className="navbar-bg-[#FA7D19] text-white px-16">
+            <div className="flex-1"> 
+                <a href="#" className="btn btn-ghost normal-case text-3xl">Mr. Robot</a>
+            </div>
+            <div className="flex-none">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default header
